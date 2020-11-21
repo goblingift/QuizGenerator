7a700cbc4a861ff2ac95201409790009be00f181
André Keßler <andre.kessler91@gmx.de>
2020-11-21 22:19:55 +0100

    Added project lombok dependencies to project

--- src/main/java/gift/goblin/quizgenerator/dto/UserCredentials.java
+++ src/main/java/gift/goblin/quizgenerator/dto/UserCredentials.java
@@ -4,32 +4,15 @@
  */
 package gift.goblin.quizgenerator.dto;
 
+import lombok.Data;
+
 /**
  *
  * @author andre
  */
+@Data
 public class UserCredentials {
     
     private String username;
 
-    public UserCredentials() {
-    }
-
-    public UserCredentials(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public String toString() {
-        return "UserCredentials{" + "username=" + username + '}';
-    }
-    
 }
